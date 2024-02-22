10f5127849fc0ee38c29d53f3b29085d65f33523
Rohini Parase <Rohini.Parase@ril.com>
2024-02-22 17:23:06 +0530

    ..

--- src/component/Lawyer/lawyerList.jsx
+++ src/component/Lawyer/lawyerList.jsx
@@ -5,8 +5,8 @@ import LawyerCard from "./LawyerCard";
 const LawyerList = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
-      {lawyer.map((lawyer, index) => (
-        <LawyerCard key={index} lawyer={lawyer} />
+      {lawyer.map((lawyer) => (
+        <LawyerCard key={lawyer.id} lawyer={lawyer} />
       ))}
     </div>
   );
